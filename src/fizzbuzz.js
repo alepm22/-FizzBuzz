5785c89bbe3d47ab4ee494b2df042c3386c95822
alejandra <alejandra.palz@ucb.edu.bo>
2024-02-26 23:03:22 -0400

    Si el número es 3 o múltiplo de 3 se imprime Fizz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,10 @@
 function generarfizzbuzz(n) {
-
-    return n + "" ;
+  let mensaje = "";
+  if(n % 3 == 0){
+      mensaje = "fizz";
+  }else{
+      mensaje = n + "";
   }
-  export default generarfizzbuzz;
+  return mensaje;
+}
+export default generarfizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,11 +1,16 @@
 import generarFizzBuzz from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
-  it("Debería retornar el mismo número para un número que no tiene reglas, por ejemplo, el 1", () => {
+  it("Deberia retornar el mismo numero para uno que no tiene reglas por ejemplo el 1", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
-
-  it("Debería retornar el mismo número para un número que no tiene reglas, por ejemplo, el 2", () => {
+  it("Deberia retornar el mismo numero para uno que no tiene reglas por ejemplo el 2", () => {
     expect(generarFizzBuzz(2)).toEqual("2");
+  });
+  it("Deberia mostrar la palabra fizz para el numero 3", () => {
+    expect(generarFizzBuzz(3)).toEqual("fizz");
+  });
+  it("Deberia mostrar la palabra fizz para los multiplos del numero 3", () => {
+    expect(generarFizzBuzz(6)).toEqual("fizz");
   });
 });
